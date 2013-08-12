eda46b1ec8d6c7d7edb22c5ad47dddc5b5c2af48
Ubuntu <ubuntu@ip-10-179-42-117.ec2.internal>
2013-08-12 06:39:31 +0000

    changed shit

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+  var text = fs.readFileSync('index.html').toString('utf-8')
+
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
